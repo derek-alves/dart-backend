425deb0d3fabdf5499fbce689a781b2fdf6e1d46
derek-alves <derek.alves1998@hotmail.com>
2022-06-30 21:38:15 -0300

    step: implements interface in publication service

---
+++ bin/services/blog/publication_service.dart
@@ -0,0 +1,28 @@
+import '../../models/models.dart';
+import '../services.dart';
+
+class PublicationService implements GenericService<Publication> {
+  @override
+  bool delete(int id) {
+    // TODO: implement delete
+    throw UnimplementedError();
+  }
+
+  @override
+  List<Publication> findAll() {
+    // TODO: implement findAll
+    throw UnimplementedError();
+  }
+
+  @override
+  Publication findOne(int id) {
+    // TODO: implement findOne
+    throw UnimplementedError();
+  }
+
+  @override
+  bool save(Publication value) {
+    // TODO: implement save
+    throw UnimplementedError();
+  }
+}
