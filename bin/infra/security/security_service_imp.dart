580b4b84f385cb828cc178459af5d9432e507742
derek-alves <derek.alves1998@hotmail.com>
2022-07-06 17:49:14 -0300

    step: create two middlewares for authentication

--- bin/infra/security/security_service_imp.dart
+++ bin/infra/security/security_service_imp.dart
@@ -1,6 +1,9 @@
 import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/src/middleware.dart';
 
-import '../../utils/custom_env.dart';
+import '../../utils/utils.dart';
+import 'authenticated_routes.dart';
 import 'security_service.dart';
 
 class SecurityServiceImp extends SecurityService<JWT> {
@@ -17,6 +20,49 @@ class SecurityServiceImp extends SecurityService<JWT> {
   @override
   Future<JWT?> validateJWT(String token) async {
     String secretKey = await CustomEnv.get(key: 'jwt_key');
-    return JWT.verify(token, SecretKey(secretKey));
+    try {
+      return JWT.verify(token, SecretKey(secretKey));
+    } on JWTInvalidError {
+      return null;
+    } on JWTExpiredError {
+      return null;
+    } on JWTNotActiveError {
+      return null;
+    } on JWTUndefinedError {
+      return null;
+    } catch (e) {
+      return null;
+    }
   }
+
+  @override
+  Middleware get authorization {
+    return (Handler handler) {
+      return (Request request) async {
+        String? authorizationHeader = request.headers['Authorization'];
+        JWT? jwt;
+
+        if (authorizationHeader?.startsWith('Bearer ') == true) {
+          String token = authorizationHeader!.substring(7);
+          jwt = await validateJWT(token);
+        }
+
+        request.change(context: {'jwt': jwt});
+        return handler(request);
+      };
+    };
+  }
+
+  @override
+  Middleware get verifyJwt => createMiddleware(
+        requestHandler: (Request request) {
+          var _authenticatedRoutes = AuthenticatedRoutes().add('login');
+
+          if (_authenticatedRoutes.authenticated(request.url.path) &&
+              request.context['jwt'] == null) {
+            return Response.forbidden("Not Authorized");
+          }
+          return null;
+        },
+      );
 }
