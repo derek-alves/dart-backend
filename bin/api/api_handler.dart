6dfab28bcf7894e0f8a53d4926a3d17fdac334c6
derek-alves <derek.alves1998@hotmail.com>
2022-07-15 16:59:15 -0300

    step: isSecurity verification to the route

--- bin/api/api_handler.dart
+++ bin/api/api_handler.dart
@@ -1,15 +1,29 @@
 import 'package:shelf/shelf.dart';
 
+import '../infra/infra.dart';
+
 abstract class ApiHandler {
-  Handler getHandler({List<Middleware>? middlewares});
+  Handler getHandler({
+    List<Middleware>? middlewares,
+    bool isSecurity = false,
+  });
 
   Handler createHandler({
     required Handler handler,
     List<Middleware>? middlewares,
+    bool isSecurity = false,
   }) {
     middlewares ??= [];
-    var pipeline = Pipeline();
 
+    if (isSecurity) {
+      var _securityService = DependencyInjector().get<SecurityService>();
+      middlewares.addAll([
+        _securityService.authorization,
+        _securityService.verifyJwt,
+      ]);
+    }
+
+    var pipeline = Pipeline();
     for (var middleware in middlewares) {
       pipeline = pipeline.addMiddleware(middleware);
     }
