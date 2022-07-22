b53003d80a83c86911095c7598d7cc24c74af46b
derek-alves <derek.alves1998@hotmail.com>
2022-07-22 12:29:31 -0300

    step: add database in dependecy injection

--- bin/infra/di/injects.dart
+++ bin/infra/di/injects.dart
@@ -1,12 +1,13 @@
 import '../../api/api.dart';
 import '../../models/models.dart';
 import '../../services/services.dart';
-import '../security/security.dart';
-import 'dependency_injector.dart';
+import '../infra.dart';
 
 class Injects {
   static DependencyInjector initialize() {
     var di = DependencyInjector();
+
+    di.register<DBConnection>(() => MySqlDbConfiguration());
 
     di.register<SecurityService>(() => SecurityServiceImp());
 
