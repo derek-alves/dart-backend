c25c64899d1427fbe23296069aecc73cd848b585
derek-alves <derek.alves1998@hotmail.com>
2022-07-30 13:02:33 -0300

    step: add login service

--- bin/api/login_api.dart
+++ bin/api/login_api.dart
@@ -1,22 +1,34 @@
-import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
 import '../infra/infra.dart';
+import '../services/services.dart';
+import '../transfer_object/transfer_object.dart';
 import 'api.dart';
 
 class LoginApi extends ApiHandler {
   final SecurityService _securityService;
+  final LoginService _loginService;
 
-  LoginApi(this._securityService);
+  LoginApi(this._loginService, this._securityService);
 
   @override
   Handler getHandler({List<Middleware>? middlewares, bool isSecurity = false}) {
     Router router = Router();
 
     router.post("/login", (Request req) async {
-      var token = await _securityService.generateJWT("1");
-      return Response.ok(token);
+      var body = await req.readAsString();
+      var userTO = AuthTransferObject.fromRequest(body);
+
+      var userID = await _loginService.authenticate(userTO);
+      if (userID > 0) {
+        var jwt = await _securityService.generateJWT(userID.toString());
+        return Response.ok(jsonEncode({'token': jwt}));
+      } else {
+        return Response(401);
+      }
     });
 
     return createHandler(
