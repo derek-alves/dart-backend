708f8a3b8cc21689c127c06732bde4e66b4e6223
derek-alves <derek.alves1998@hotmail.com>
2022-07-31 11:44:13 -0300

    step: implements all methods

--- bin/services/publication/publication_service.dart
+++ bin/services/publication/publication_service.dart
@@ -1,28 +1,32 @@
 import '../../models/models.dart';
+import '../../repositories/publication_repository.dart';
 import '../services.dart';
 
 class PublicationService implements GenericService<Publication> {
+  final PublicationRepository _publicationRepository;
+
+  PublicationService(this._publicationRepository);
   @override
   Future<bool> delete(int id) {
-    // TODO: implement delete
-    throw UnimplementedError();
+    return _publicationRepository.delete(id);
   }
 
   @override
   Future<List<Publication>> findAll() {
-    // TODO: implement findAll
-    throw UnimplementedError();
+    return _publicationRepository.findAll();
   }
 
   @override
-  Future<Publication> findOne(int id) {
-    // TODO: implement findOne
-    throw UnimplementedError();
+  Future<Publication?> findOne(int id) {
+    return _publicationRepository.findOne(id);
   }
 
   @override
-  Future<bool> save(Publication value) {
-    // TODO: implement save
-    throw UnimplementedError();
+  Future<bool> save(Publication value) async {
+    if (value.id != null) {
+      return await _publicationRepository.update(value);
+    } else {
+      return await _publicationRepository.create(value);
+    }
   }
 }
